a180292efce28ca7a33ca047ca824c6b573033f6
ruslan <tsasiuk.ruslan@chnu.edu.ua>
2022-12-19 23:15:24 +0200

    link changed

--- demo.rb
+++ demo.rb
@@ -2,7 +2,7 @@ require 'csv'
 require './Parser.rb'
 require './Cart.rb'
 
-parser = Parser.new('https://vsedlasna.com/matrasy/')
+parser = Parser.new('https://rozetka.com.ua/ua/notebooks/c80004/')
 
 cart = Cart.new()
 
